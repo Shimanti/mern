228f70907942e326a6ea3c80646b600b0a33de8b
Shimanti <shimanti@gmail.com>
2021-07-30 00:53:29 -0700

    CORs code added to app.js

--- app.js
+++ app.js
@@ -4,19 +4,33 @@
 // express is our main framework
 // mongoose is used to connect/interact with MongoDB
 // validation (as its name implies) is used for validation
+// cors is to prevent block
 
 // app.js
+
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
 
 const app = express();
 
 // Connect Database
 connectDB();
 
-// index
+// Cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Hello world!'));
 
-// PORT
+// Use Routes
+app.use('/api/books', books);
+
 const port = process.env.PORT || 8082;
-app.listen(port, () => console.log(`Server running on port ${port}`))
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
